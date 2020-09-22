5c437e1fcc6d44dd91921de2ed49a2021c217574
Bryce Moore <brycemooore@gmail.com>
2020-09-22 20:38:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_key = ''
   name_hash.each{|key, num|
-    min = name_hash[] 
+    min = name_hash.first 
+    puts min
     
     
   }
